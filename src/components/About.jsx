40e174628904561f0f6ea246920d9e8e7feae589
Amar-Gupta-721 <amargupta721@gmail.com>
2024-08-18 19:01:22 +0530

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,6 +26,19 @@ const About = () => {
                         <p className='my-2 max-w-xl'>Hello! I'm Amar Gupta, pursuing a B.Tech in Computer Science and Engineering with a passion for frontend development. I build dynamic, responsive websites using HTML, CSS, JavaScript, React, and other modern technologies <br />
                         I am dedicated to my work and always ready to take on new challenges with a positive attitude. My strengths include a strong willingness to learn, dedication towards my work, and the ability to approach new challenges with a positive mindset. I am constantly seeking opportunities to grow and make meaningful contributions.
                         </p>
+                        
+                    </div>
+                    <div>
+                        <button className='font-bold border border-neutral-50 rounded bg-neutral-900 px-2 py-1 text-neutral-50 hover:bg-white hover:text-neutral-900'>
+                            <a 
+                                href="/AmarGuptaResume.pdf" 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                className='text-inherit'>
+                                View My Resume
+                            </a>
+                        </button>
+
                     </div>
             </motion.div>
         </div>
